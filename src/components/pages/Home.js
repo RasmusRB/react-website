c4022269b7008931a5412844f18b8e2ceef3f452
Rasmus Bergstrøm <rasmusbergstroem87@gmail.com>
2021-08-16 21:29:51 +0200

    asd

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import '../../App.css';
+import HeroUnit from '../HeroUnit';
+import Cards from '../Cards';
+
+function Home(){
+    return (
+        <div>
+            <HeroUnit />
+            <Cards />
+        </div>
+        );
+}
+
+export default Home;
